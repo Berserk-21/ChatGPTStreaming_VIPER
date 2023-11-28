0a9a39e690b613e038e8d7e6176b43d1179aa59f
Berserk-21 <cryptogekkofr@protonmail.com>
2023-11-28 15:11:10 +0100

    initialized xcode project

---
+++ ChatGPTStreaming_VIPER/ChatGPTStreaming_VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatGPTStreaming_VIPER
+//
+//  Created by Berserk on 28/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
